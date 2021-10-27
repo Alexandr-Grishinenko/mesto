920189d433a59b73157b8c188d9d5e57c0ed373e
Александр Гришиненко <@KZ024.kaztour.lan>
2021-10-27 14:22:04 +0600

    для сравнения

--- scripts/script.js
+++ scripts/script.js
@@ -1,60 +1,71 @@
 let content = document.querySelector('.content');
+
+// Объявление переменных - Кнопки открытия/закрытия pop-up
 let editButton = content.querySelector('.profile__edit-button');
-let editPopup = content.querySelector('.popup-for-edit');
 let closeButton = content.querySelector('.popup-for-edit__close-button');
 
+let editPopup = content.querySelector('.popup-for-edit');
+
+// Объявление переменных - Имя и описание аккаунта на странице
 let accountNameOnThePage = content.querySelector('.profile__account-name');
 let accountDescrOnThePage = content.querySelector('.profile__account-description');
 
+// Объявление переменных - Форма редактирования имени и описания аккаунта
 let formElement = content.querySelector('.popup-for-edit__form');
 
+// Объявление переменных - Инпуты для редактирования имени и описания аккаунта в форме
 let nameInput = formElement.querySelector('.popup-for-edit__form-item_value_name');
 let jobInput = formElement.querySelector('.popup-for-edit__form-item_value_description');
 
+// Открытие pop-up; Присвоение инпутам значений имени и описания аккаунта, находящихся на странице
+editButton.addEventListener('click', function() {
+    editPopup.classList.add('popup-for-edit_opened');
+    nameInput.setAttribute('value', accountNameOnThePage.textContent);
+    jobInput.setAttribute('value', accountDescrOnThePage.textContent);
+  }
+);
 
-function popupOpening() {
-  editPopup.classList.add('popup-for-edit_opened');
-  nameInput.setAttribute('value', accountNameOnThePage.textContent);
-  jobInput.setAttribute('value', accountDescrOnThePage.textContent);
+//Закрытие pop-up по нажатию на крестик
+closeButton.addEventListener('click', function() {
+    editPopup.classList.remove('popup-for-edit_opened');
+  }
+);
 
-  console.log(nameInput.getAttribute('value'));
-  console.log(jobInput.getAttribute('value'));
-}
-
-function popupClosing() {
-  editPopup.classList.remove('popup-for-edit_opened');
-}
-
-editButton.addEventListener('click', popupOpening);
-closeButton.addEventListener('click', popupClosing);
-
-
-console.log(nameInput.getAttribute('value'));
-console.log(jobInput.getAttribute('value'));
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                          // Так мы можем определить свою логику отправки.
-                          // О том, как это делать, расскажем позже.
-
-
-    accountNameOnThePage.textContent = nameInput.getAttribute('value');
-    // nameInput.getAttribute('value')
-    accountDescrOnThePage.textContent = jobInput.getAttribute('value');
-    // jobInput.getAttribute('value')
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    console.log(nameInput.getAttribute('value'));
-    console.log(jobInput.getAttribute('value'));
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+// Закрытие pop-up по нажатию на любое место в окне, кроме контейнера формы
+// Нравится, когда всплывающие окна можно закрыть нажав на любое свободное место
+// Поискал в нете, спросил у коллег-программистов - подсказали, объяснили
+editPopup.addEventListener('click', function(evt) {
+    if (evt.target !== this) {
+      return;
+    }
     popupClosing();
-}
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', formSubmitHandler);
+  }
+);
+
+// Функция - Логика редактирования значений имени и описания аккаунта через форму
+formElement.addEventListener('submit', function(evt) {
+    evt.preventDefault(); 
+
+    accountNameOnThePage.textContent = nameInput.value;
+    accountDescrOnThePage.textContent = jobInput.value;
+
+    popupClosing();
+  }
+);
+
+// Изменение отображения кнопки like после нажатия
+let likeButton = content.querySelector('.card__like-button');
+likeButton.addEventListener('click', function() {
+    if (this.classList.contains('card__like-button_liked')) {
+      this.classList.remove('card__like-button_liked')
+    } else {
+      this.classList.add('card__like-button_liked')
+    }
+  }
+);
+
+
+
+
+
+
